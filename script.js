e1e41fd680ce936e22d46600522916e3ae7dc8ea
Tiago Gomes <Gomes.gt001@gmail.com>
2024-02-05 15:50:54 -0300

    conclusão projeto lâmpada

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-const turnOn = document.getElementById('turnOn')
+const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById ('turnOff');
+const lamp = document.getElementById ('lamp');
+
+function islampBroken () {
+    return lamp.src.indexOf ('quebrada') > - 1
+}
+
+function lampOn () {
+    if (!islampBroken () ){
+        lamp.src = './img/ligada.jpg';
+    }
+}
+
+function lampOff () {
+    if (!islampBroken () ){
+        lamp.src ='./img/desligada.jpg';
+    }
+}
+
+function lampBroken () {
+    lamp.src = './img/quebrada.jpg';
+}
+turnOff.addEventListener ('click', lampOff);
+turnOn.addEventListener ('click', lampOn);
+lamp.addEventListener ('mouseover', lampOn);
+lamp.addEventListener ('mouseleave', lampOff)
+lamp.addEventListener ('dblclick', lampBroken)
